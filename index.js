d3e1f286ef9a9fa10c616d4aed53e2cfcb733900
haiduongdana <haiduongdana@gmail.com>
2022-11-15 17:06:28 +0700

    change page

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ var dataTable = {
     CONSTS: {},
   
     SELECTORS: {
-        table: ".data-table-container table",
+        table: ".data-table-container table tbody",
         table_item: "tr td",
         paginate_button: ".paginate-button",
         prev_button: ".paginate-button .prev-button",
         next_button: ".paginate-button .next-button",
-        page_button: ".paginate-button .page-button"
+        page_button: ".paginate-button .page-button",
+        select_entries: "elect"
     },
 
     init: function () {
-        dataTable.renderTable(1);
-        dataTable.renderPageButton(10);
-        // dataTable.pageChangeEvent();
+        dataTable.renderTable();
+        dataTable.renderPageButton();
+        dataTable.pageChangeEvent();
+        dataTable.selectEvent();
     },
 
-    renderTable: function(numPage) {
+    renderTable: function() {
         let html = "";
-        let index = (numPage-1) * 10;
-        let maxIndex = Number(index) + 10
-        while(index < maxIndex) {
+        let index = (dataTable.GLOBAL.currentPage-1) * dataTable.GLOBAL.itemPerPage;
+        let dem = dataTable.GLOBAL.itemPerPage;
+        while(dem > 0) {
             html += `<tr>
                 <td>${dataTable.GLOBAL.listData[index].rendering_engine}</td>
                 <td>${dataTable.GLOBAL.listData[index].browser}</td>
@@ -38,42 +40,55 @@ var dataTable = {
                 <td>${dataTable.GLOBAL.listData[index].css_grade}</td>
             </tr>`
             index ++;
+            dem--;
+            if(index >= dataTable.GLOBAL.listData.length) break;
         }
-        $(dataTable.SELECTORS.table).append(html);
+        $(dataTable.SELECTORS.table).html(html);
     },
 
-    renderPageButton: function(itemPerPage) {
-        let allPage = dataTable.GLOBAL.listData.length / itemPerPage;
-        allPage = Math.floor(allPage) + 1
+    renderPageButton: function() {
+        dataTable.GLOBAL.allPage = dataTable.GLOBAL.listData.length / dataTable.GLOBAL.itemPerPage;
+        dataTable.GLOBAL.allPage = Math.round(dataTable.GLOBAL.allPage);
         let html = "";
         html += ` <li>
-                    <a class ="pre-button"href="">Previous</a>
+                    <a class ="prev-button">Previous</a>
                 </li>`;
-        for(let i = 0; i < allPage; i++) {
+        for(let i = 0; i < dataTable.GLOBAL.allPage; i++) {
             html += `<li>
-                        <a class="page-button" href="" index="${i}">${i+1}</a>
+                        <a class="page-button" index="${i}">${i+1}</a>
                     </li>`
         }
         html += `<li>
-                    <a class ="next-button"href="">Next</a>
+                    <a class ="next-button">Next</a>
                 </li>`
         $(dataTable.SELECTORS.paginate_button).html(html);
     },
 
-    // pageChangeEvent: function() {
-    //     $(dataTable.SELECTORS.next_button).on("click", function () {
-    //         let nextPage = dataTable.GLOBAL.currentPage >= dataTable.GLOBAL.allPage ? dataTable.GLOBAL.currentPage : Number(dataTable.GLOBAL.currentPage) + 1;
-    //         dataTable.GLOBAL.currentPage = nextPage;
-    //         dataTable.renderTable(dataTable.GLOBAL.currentPage);
-    //     })
-    //     // $(dataTable.SELECTORS.prev_button).on("click", function () {
-    //     //     nextPage = dataTable.GLOBAL.currentPage <= 1 ? currentPage : Number(dataTable.GLOBAL.currentPage) - 1;
-    //     // })
-    //     // $(dataTable.SELECTORS.page_button).on("click", function() {
-    //     //     nextPage = $(this).attr("index") + 1;
-    //     // })
-        
-    // }
+    pageChangeEvent: function() {
+        $(dataTable.SELECTORS.next_button).on("click", function () {
+            let nextPage = dataTable.GLOBAL.currentPage >= dataTable.GLOBAL.allPage ? dataTable.GLOBAL.currentPage : Number(dataTable.GLOBAL.currentPage) + 1;
+            dataTable.GLOBAL.currentPage = nextPage;
+            dataTable.renderTable();
+        })
+        $(dataTable.SELECTORS.prev_button).on("click", function () {
+            let nextPage = dataTable.GLOBAL.currentPage <= 0 ? 0 : Number(dataTable.GLOBAL.currentPage) - 1;
+            dataTable.GLOBAL.currentPage = nextPage;
+            dataTable.renderTable();
+        })
+        $(dataTable.SELECTORS.page_button).on("click", function() {
+            let nextPage = Number($(this).attr("index")) + 1;
+            dataTable.GLOBAL.currentPage = nextPage;
+            dataTable.renderTable();
+        })
+
+    },
+
+    selectEvent: function() {
+        $(dataTable.SELECTORS.select_entries).change(function() {
+            dataTable.GLOBAL.itemPerPage = $(dataTable.SELECTORS.select_entries).find(":selected").val();
+            dataTable.renderTable();
+        })
+    }
 
 
 };
